543c2d4a8434c526e918c3c6e5364b771d4101a0
JackKorogi <jackkorogi@gmail.com>
2025-04-24 15:36:15 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,57 +1,62 @@
-//TODO: fetch data from the PostgreSQL database (to be implemented later)
-function fetchGradeData(){
-    //this function will query the PostgreSQL database and return grade data
+// TODO: Fetch data from the PostgreSQL database (to be implemented later)
+function fetchGradeData() {
+    // This function will query the PostgreSQL database and return grade data
     console.log("Fetching grade data...");
-    //create a new request for HTTP data
+    // Create a new request for HTTP data
     let xhr = new XMLHttpRequest();
-    //this is the address on the machine we're asking for data
+    // This is the address on the machine we're asking for data
     let apiRoute = "/api/grades";
-    //when the request changes status, we run this anonymous function
-    xhr.onreadystatechange = function(){
+    // When the request changes status, we run this anonymous function
+    xhr.onreadystatechange = function () {
         let results;
-        //check if we're done
-        if(xhr.readyState === xhr.DONE){
-            //check if we're successful
-            if(xhr.status !== 200){
+        // check if we're done
+        if (xhr.readyState === xhr.DONE) {
+            // check if we're successful
+            if (xhr.status !== 200) {
                 console.error(`Could not get grades.
-                    Status: ${xhr.status}`)
+                     Status: ${xhr.status}`);
             }
-            //and then call the function to update the HTML with our data
+            // And the call the function to update the HTML with our data
             populateGradebook(JSON.parse(xhr.responseText));
         }
     }.bind(this);
-    xhr.open("get", apiRoute, true);
+    xhr.open("GET", apiRoute, true);
     xhr.send();
 }
 
-//TODO: populate the table with grade data
-function populateGradebook(data){
-    //this function will take the fetched grade data and populate the table
-    console.log("Populating gradebook with data:", data);
-    let tableElm = document.getElementById("gradebook"); //get the gradebook table element
-        data.forEach(function(assignment){ //for each row of data we're passed in
-            let row = document.createElement('tr'); //create a table row element
-            let columns = []; //handy place to stick the columns of information
-            columns.name = document.createElement('td'); //the first column's table data will be the name
-            columns.name.appendChild(
-                //concatenate the full name: "last_name, first_name"
-                document.createTextNode(assignment.last_name + ", " + assignment.first_name)
-            );
-            columns.grade = document.createElement('td'); //second column will be the grade
-            columns.grade.appendChild(
-                //just put the name in text, you could be fancy and figure out the letter grade here with either a bunch of conditions, or a javascript "switch" statement
-                document.createTextNode(assignment.total_grade)
-            );
-            //add the table data columns to the table row
-            row.appendChild(columns.name);
-            row.appendChild(columns.grade);
-            //add the row to the table itself to make the data visible
-            tableElm.appendChild(row);
-        });
-}
 
-//TODO REMOVE THIS
-//Call the stubs to demonstrate the workflow
-const gradeData = fetchGradeData();
-populateGradebook(gradeData);
-//END REMOVE
+// TODO: Populate the table with grade data
+function populateGradebook(data) {
+    // This function will take the fetched grade data and populate the table
+    console.log("Populating gradebook with data:", data);
+    let tableElm = document.getElementById("gradebook");// Get the gradebook table element
+       data.forEach(function(assignment){ // For each roll of data we're passed in
+        let row = document.createElement("tr"); // Create a new table row element
+        let columns =[]; // Handy place to stick the columns of information
+        columns.name = document.createElement("td"); // The first column's table data will be the name
+        // concatenate the full name: "last_name, first_name"
+        columns.name.appendChild(
+            document.createTextNode(assignment.last_name + ", " + assignment.first_name)
+        );
+        columns.grade = document.createElement("td"); // second column will be the grade
+        columns.grade.appendChild(
+            // Just put the name in text, you could be fancy and figure out the letter grade here
+            // with either a bunch of conditions, or a JavaScript  "switch" statement
+            document.createTextNode(assignment.total_grade)
+        );
+        // Add the table data columns to the table row
+        row.appendChild(columns.name); // Append the name column to the row
+        row.appendChild(columns.grade); // Append the grade column to the row
+        // Add the table data columns to the table row
+            tableElm.appendChild(row); // Append the row to the table
+        });
+    }
+    fetchGradeData()
+
+// This part sets up the database
+const {Pool} = require('pg');
+// You may need to modify the password or database name in the following line:
+const connectionString = `postgres://postgres:CTI_110_WakeTech@localhost/Gradebook_halliday`;
+// The default password is CTI_110_WakeTech
+// The default database name is Gradebook
+const pool = new Pool({connectionString:connectionString})
